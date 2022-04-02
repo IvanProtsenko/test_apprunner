d09e4cc50b9610e923c0563f9e22cf287c58a914
IvanProtsenko <ivnprotsenko@gmail.com>
2022-04-02 14:23:40 +0300

    new

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello world');
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  return new Response(request.body, {
+    headers: { 'content-type': 'text/plain' },
+  });
+}
